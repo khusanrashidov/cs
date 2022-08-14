31b10cf49d2881015e9c223d632a0f56251443a6
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-08-14 23:18:40 +0500

    Add files via upload

---
+++ 00.MyFirstProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MyFirstProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("I like C#");
+			Console.WriteLine(" or JS?");
+			Console.WriteLine("Hello World!");
+			Console.Beep(); //for beep
+			//press cw and press tab twice for Console.WriteLine() shortcut
+			/*
+			 * \t for tab escape sequence
+			 * \b for backspace
+			 * \n for newline
+			*/
+			Console.ReadKey(); //pause till any key is pressed
+		}
+	}
+}
