05d0902258a9493e6a4bb7a3ee820f01204a404a
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-08-15 13:33:10 +0500

    Add files via upload

---
+++ 03.TypeCasting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace TypeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * type casting - converting a value to a different data type,
+             * it is useful when we accept user input (string). As different
+             * data types can do different things.
+             */
+            double a = 3.14159265;
+            int b = Convert.ToInt32(a); //or 16 instead 32 but not 64
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(a);
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(b);
+            int K = 8;
+            Console.WriteLine();
+            double H = Convert.ToDouble(K) + 0.8; //typecasting to double
+            Console.WriteLine(K.GetType());
+            Console.WriteLine(K);
+            Console.WriteLine(H.GetType());
+            Console.WriteLine(H);
+            Console.WriteLine();
+            double X = 8.1;
+            string S = Convert.ToString(X);
+            Console.WriteLine(X.GetType());
+            Console.WriteLine(X);
+            Console.WriteLine(S.GetType());
+            Console.WriteLine(S);
+            Console.WriteLine();
+            string s = "*";
+            char c = Convert.ToChar(s);
+            Console.WriteLine(s.GetType());
+            Console.WriteLine(s);
+            Console.WriteLine(c.GetType());
+            Console.WriteLine(c);
+            Console.WriteLine();
+            string str = "true";
+            bool boolean = Convert.ToBoolean(str);
+            Console.WriteLine(str.GetType());
+            Console.WriteLine(str);
+            Console.WriteLine(boolean.GetType());
+            Console.WriteLine(boolean);
+            Console.ReadKey();
+        }
+    }
+}
