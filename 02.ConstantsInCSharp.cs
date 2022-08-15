298ce1036d44bcd7f548b6219b7ebb94eed5669f
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-08-15 13:27:47 +0500

    Add files via upload

---
+++ 02.ConstantsInCSharp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ConstantsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * constants - immutable values which are known at compile
+             * time and do not change for the life of the program.
+             */
+            const double pi = 3.14159265;
+            Console.WriteLine(pi);
+            Console.ReadLine();
+        }
+    }
+}
