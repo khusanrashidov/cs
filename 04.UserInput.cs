f3d40d2b931381f8194235c27e0a6155e7751a4f
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-08-15 13:36:00 +0500

    Add files via upload

---
+++ 04.UserInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your full name: ");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("Your full name: " + fullname);
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is " + age);
+            Console.Write("Enter your favourite number: ");
+            double K = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your favourite number is {0}", K);
+            Console.ReadKey();
+        }
+    }
+}
