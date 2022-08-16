c510aac9c5abb1557a02d31ecb911b5e5cc1fc62
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-08-16 15:34:36 +0500

    Add files via upload

---
+++ 08.HypotenuseCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HypotenuseCalculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter the value of the leg 1: ");
+			double leg1 = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Enter the value of the leg 2: ");
+			double leg2 = Convert.ToDouble(Console.ReadLine());
+			double hypothenuse = Math.Round(Math.Sqrt(Math.Pow(leg1, 2) + Math.Pow(leg2, 2)), 2);
+			Console.Write("The hypotenuse of the right triangle: {0}", hypothenuse);
+			Console.ReadKey();
+		}
+	}
+}
