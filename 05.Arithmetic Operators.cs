3c8e24f0c40ee0796390c42da94e53457754217c
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-08-16 08:47:24 +0500

    Add files via upload

---
+++ 05.Arithmetic Operators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Arithmetic_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 8.0;
+            number = number + number;
+            number += number;
+            number++;
+            number--;
+            number *= 2;
+            number %= 56;
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+    }
+}
