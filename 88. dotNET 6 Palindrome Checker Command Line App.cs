b0723ab121449db7ad42d1eb2d6b89a24ef9a8c6
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-08-17 22:26:44 +0500

    Add files via upload

---
+++ 88. dotNET 6 Palindrome Checker Command Line App.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+(bool, int) PalindromeChecker(string Str)
+{
+    Str = Str.ToLower();
+    var sb = new StringBuilder();
+    int i;
+    foreach (char c in Str)
+    {
+        if (char.IsPunctuation(c) || char.IsWhiteSpace(c))
+        {
+            continue;
+        }
+        sb.Append(c);
+    }
+    string tempStr = sb.ToString();
+    int begin = 0;
+    int end = tempStr.Length - 1;
+    for (; begin <= end; begin++, end--)
+    {
+        if (tempStr[begin] != tempStr[end])
+            return (false, 0);
+    }
+    return (true, Str.Length);
+}
+string stringInput = "";
+(bool, int) result;
+Console.WriteLine("Let's begin:");
+while (true)
+{
+    stringInput = Console.ReadLine();
+    if (stringInput == "exit")
+        break;
+    result = PalindromeChecker(stringInput);
+    Console.WriteLine($"Palindrome: {result.Item1}, Length: {result.Item2}");
+}
