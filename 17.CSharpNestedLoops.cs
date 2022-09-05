5fe876a3afe5d815bd2e7cac9fc862a8cf465d3c
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-09-05 06:07:49 +0500

    Add files via upload

---
+++ 17.CSharpNestedLoops.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CSharpNestedLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * nested loops = loops inside of other loops
+             * uses vary, used a lot in sorting algorithms
+             */
+            Console.Write("How many rows?: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("How many columns?: ");
+            int column = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter any character symbol: ");
+            char @char = (char)Console.Read();
+            Console.WriteLine();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write(" " + @char);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
