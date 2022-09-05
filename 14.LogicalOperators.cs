1e949f5f47c20330a051e39ade60c256a87b5e64
Khusan Rashidov <89833069+khusanrashidov@users.noreply.github.com>
2022-09-05 06:03:31 +0500

    Add files via upload

---
+++ 14.LogicalOperators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //logical operators = can be used to check if more than one condition is true/false
+            /*
+             * && (AND)
+             * || (OR)
+             */
+            Console.Write("What's the temperature outside in Celsius: ");
+            double temperature = Convert.ToDouble(Console.ReadLine());
+            if (temperature >= 10 && temperature <= 25)
+            {
+                Console.WriteLine("It's warm outside.");
+            }
+            else if (temperature <= -80 || temperature >= +80)
+            {
+                Console.WriteLine("Extreme temperature, lethal for humans.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
